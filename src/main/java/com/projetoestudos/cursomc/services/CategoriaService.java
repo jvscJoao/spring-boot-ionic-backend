5ff5e9338ec64d8519f506da436752f51407bebe
jvscJoao <joaov1500@gmail.com>
2021-11-08 22:21:19 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/projetoestudos/cursomc/services/CategoriaService.java
+++ src/main/java/com/projetoestudos/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.projetoestudos.cursomc.domain.Categoria;
+import com.projetoestudos.cursomc.dto.CategoriaDTO;
 import com.projetoestudos.cursomc.repositories.CategoriaRepository;
 import com.projetoestudos.cursomc.services.exceptions.DataIntegrityException;
 import com.projetoestudos.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
