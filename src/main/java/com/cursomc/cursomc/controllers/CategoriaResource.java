d7f9c0e3b09dc0af69fefd52c1da12c497f3e62c
jvscJoao <joaov1500@gmail.com>
2024-01-10 18:50:59 -0300

    Alterando nome da pasta

--- src/main/java/com/cursomc/cursomc/controllers/CategoriaResource.java
+++ src/main/java/com/cursomc/cursomc/controllers/CategoriaResource.java
@@ -1,4 +1,4 @@
-package com.cursomc.cursomc.resources;
+package com.cursomc.cursomc.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
