1a36667135976e3fd0f31bb989e49b23470e1963
jvscJoao <joaov1500@gmail.com>
2024-02-13 18:10:14 -0300

    Atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/cursomc/cursomc/controllers/CategoriaController.java
+++ src/main/java/com/cursomc/cursomc/controllers/CategoriaController.java
@@ -14,6 +14,8 @@ import com.cursomc.cursomc.domain.Categoria;
 import com.cursomc.cursomc.services.CategoriaService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -25,18 +27,25 @@ public class CategoriaController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> find(@PathVariable Integer id) {
-        Categoria obj = service.buscar(id);
+        Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
 
     @PostMapping()
-    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+    public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
         .path("/{id}")
         .buildAndExpand(obj.getId())
         .toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update (@RequestBody Categoria obj, @PathVariable Integer id) {
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
     }
      
 }
